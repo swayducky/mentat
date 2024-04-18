6ac835079943c979c5f553258bb38cd77c02e8bc
Sway Ducky <swayducky@gmail.com>
2024-04-18 16:38:08 -0700

    install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,10 @@ fi
 
 pip install --upgrade pip
 pip install -e .
+
+mkdir -p ~/.mentat
+cd ~/.mentat
+CONFIG_FILE=~/gd/_util/.mentat_config.json
+ln -sf $CONFIG_FILE .
+echo "== Contents of: $CONFIG_FILE"
+cat $CONFIG_FILE
